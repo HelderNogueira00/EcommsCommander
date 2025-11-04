5c2898babc24f3ce179e302ffc5c456070696a1a
Helder Nogueira <heldernogueira.dev@gmail.com>
2025-11-04 05:25:38 -0500

    cleaning up dirs and moving to gradle :)

--- EnvironmentVars.java
+++
@@ -1,8 +0,0 @@
-public final class EnvironmentVars {
-    
-    public static final String TokenFile = "/ecomms/config/token.enc";
-    public static final String KeystoreFile = "/ecomms/config/client.p12";
-    public static final String CommandsFile = "/ecomms/config/command.txt";
-    public static final String TruststoreFile = "/ecomms/config/truststore.p12";
-    public static final String KeystorePasswordFile = "/ecomms/config/kspwd.enc";
-}
